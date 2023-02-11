bfb8946d3191d5a0faf14d0a97bc0088f4126285
IT-Engineer-K <76894075+IT-Engineer-K@users.noreply.github.com>
2023-02-12 00:08:20 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -1,17 +1,17 @@
 from flask import Flask, request
 import platform
 import encoder
+import gc
 
 app = Flask(__name__)
 
-
 @app.route('/', methods=['GET', 'POST'])
-def route():
+async def route():
     if request.method == 'POST':
         print(request.form['text'])
         music_name = encoder.getMusic(request.form['text'])
         return music_name
-    return 'OK'
+    return str(gc.collect())
 
 
 @app.after_request
@@ -26,6 +26,3 @@ def after_request(response):
     response.headers.add('Access-Control-Allow-Credentials', 'true')
     return response
 
-
-if __name__ == '__main__' and platform.system() == 'Windows':
-    app.run(debug=True)
--- encoder.py
+++ encoder.py
@@ -1,35 +1,27 @@
 import numpy as np
-from transformers import BertJapaneseTokenizer, BertModel
-
+from transformers import BertTokenizer, BertModel
 import torch
 
 MODEL_NAME = 'model'
 
 #トークナイザとモデルのロード
-tokenizer = BertJapaneseTokenizer.from_pretrained(MODEL_NAME)
+tokenizer = BertTokenizer.from_pretrained(MODEL_NAME)
 bertModel = BertModel.from_pretrained(MODEL_NAME)
 
-def encode(text):
-    encoding = tokenizer(
-        text,
-        return_tensors = 'pt'
-        )
-    attention_mask = encoding['attention_mask']
-    
-    #文章ベクトルを計算
-    with torch.no_grad():
-        output = bertModel(**encoding)
-        last_hidden_state = output.last_hidden_state
-        averaged_hidden_state =(last_hidden_state*attention_mask.unsqueeze(-1)).sum(1)/attention_mask.sum(1,keepdim=True) 
-    return 'おとぼけダンス'
-    sentence_vectors = []
-    #文章ベクトルとラベルを追加
-    sentence_vectors.append(averaged_hidden_state[0].cpu().numpy())
 
-    #ベクトルとラベルをnumpy.ndarrayにする
-    sentence_vectors = np.vstack(sentence_vectors)
-    return sentence_vectors
+def toVec(text):
+    input_batch = [text]
 
+    encoded_data = tokenizer.batch_encode_plus(
+    input_batch, pad_to_max_length=True, add_special_tokens=True)
+
+    input_ids = torch.tensor(encoded_data["input_ids"])
+    tokenizer.convert_ids_to_tokens(input_ids[0].tolist())
+
+    outputs = bertModel(input_ids)
+    last_hidden_states = outputs[0]
+    sentencevec = last_hidden_states[:,0,:]
+    return sentencevec.detach().numpy()
 
 musics = [
 'おとぼけダンス', '大混乱', 'Funny_Funny', '全力で逃げる時のBGM', 'トッカータとフーガ〜ギャグVer〜', 'シラけムードは少し気まずい',
@@ -41,6 +33,7 @@ from tensorflow.keras.models import load_model
 
 model = load_model('model.h5')
 def getMusic(sentence):
-    predicted = model(np.array(encode(sentence)))
+    print(sentence)
+    predicted = model(np.array(toVec(sentence)))
     index = np.argmax(predicted[0])
     return musics[index]
